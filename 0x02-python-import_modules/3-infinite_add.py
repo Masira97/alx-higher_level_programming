17a32fc97a0a64967ad594c75da3846629e27766
Masira97 <naashmasira@gmail.com>
2022-12-01 12:36:15 -0800

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
 
+
+
 if __name__ == "__main__":
+
+    """Print the number of and list of arguments."""
 
     import sys
 
-    result = sum(int(sys.argv[x]) for x in range(1, len(sys.argv)))
 
-    print("{}".format(result))
+
+    count = len(sys.argv) - 1
+
+    if count == 0:
+
+        print("0 arguments.")
+
+    elif count == 1:
+
+        print("1 argument:")
+
+    else:
+
+        print("{} arguments:".format(count))
+
+    for i in range(count):
+
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
